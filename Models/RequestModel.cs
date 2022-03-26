c353beb97c04f3e4036880912b04bf6860202ba9
amzaandreea <amza.andreea23@yahoo.com>
2022-03-26 16:23:16 +0200

    Finished minimal agent and added try catches Needs refactoring for codes different than 200

---
+++ Models/RequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FileAgent.Models
+{
+    public class RequestModel
+    {
+        [JsonPropertyName("path")]
+        public string PathString { get; set; }
+
+        [JsonPropertyName("content")]
+        public string FileContent { get; set; }
+
+        [JsonPropertyName("type")]
+        public string FileType { get; set; }
+    }
+}
