50ed6cd9d4e3d479867a2121391ddbe2b6e787f5
amzaandreea <amza.andreea23@yahoo.com>
2022-03-26 13:59:33 +0200

    Tested get all files endpoint

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using FileAgent.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,21 +11,27 @@ using Microsoft.Extensions.Logging;
 
 namespace FileAgent.Controllers
 {
+    public class Path
+    {
+        public string PathString { get; set; }
+    }
+
     [ApiController]
     [Route("[controller]")]
     public class FilesController : ControllerBase
     {
         [HttpGet("GetItemsAtPath")]
-        public IActionResult GetItemsAtPath(string path)
+        
+        public IActionResult GetItemsAtPath([FromBody] Path path)
         {
-            List<String> files = Directory.GetFiles(path).ToList();
-            List<String> directories = Directory.GetDirectories(path).ToList();
+            List<String> files = Directory.GetFiles(path.PathString).ToList();
+            List<String> directories = Directory.GetDirectories(path.PathString).ToList();
             if (files.Count > 0 || directories.Count > 0)
             {
                 ListingModel listingModel = new ListingModel();
                 listingModel.Files = files;
                 listingModel.Directories = directories;
-                return Ok(listingModel);
+                return Ok(JsonSerializer.Serialize(listingModel));
             }
 
             return NotFound();
