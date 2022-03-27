eb72c7a471cff1773533d209e3249c4cb819f6ff
amzaandreea <amza.andreea23@yahoo.com>
2022-03-27 05:55:26 +0300

    deleted a nuget

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -177,47 +177,47 @@ namespace FileAgent.Controllers
             }
         }
 
-        public double GetUsage(Process process)
-        {
-            // Getting information about current process
-            //var process = Process.GetCurrentProcess();
+        //public double GetUsage(Process process)
+        //{
+        //    // Getting information about current process
+        //    //var process = Process.GetCurrentProcess();
 
-            // Preparing variable for application instance name
-            var name = string.Empty;
+        //    // Preparing variable for application instance name
+        //    var name = string.Empty;
 
-            foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
-            {
-                if (instance.StartsWith(process.ProcessName))
-                {
-                    using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
-                    {
-                        if (process.Id == (int)processId.RawValue)
-                        {
-                            name = instance;
-                            break;
-                        }
-                    }
-                }
-            }
+        //    foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
+        //    {
+        //        if (instance.StartsWith(process.ProcessName))
+        //        {
+        //            using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
+        //            {
+        //                if (process.Id == (int)processId.RawValue)
+        //                {
+        //                    name = instance;
+        //                    break;
+        //                }
+        //            }
+        //        }
+        //    }
 
-            var cpu = new PerformanceCounter("Process", "% Processor Time", name, true);
-           // var ram = new PerformanceCounter("Process", "Private Bytes", name, true);
+        //    var cpu = new PerformanceCounter("Process", "% Processor Time", name, true);
+        //   // var ram = new PerformanceCounter("Process", "Private Bytes", name, true);
 
-            // Getting first initial values
-            cpu.NextValue();
-            //ram.NextValue();
+        //    // Getting first initial values
+        //    cpu.NextValue();
+        //    //ram.NextValue();
 
-            // Creating delay to get correct values of CPU usage during next query
-            Thread.Sleep(500);
+        //    // Creating delay to get correct values of CPU usage during next query
+        //    Thread.Sleep(500);
 
-            dynamic result = new ExpandoObject();
+        //    dynamic result = new ExpandoObject();
 
-            // If system has multiple cores, that should be taken into account
-            result.CPU = Math.Round(cpu.NextValue() / Environment.ProcessorCount, 2);
-            // Returns number of MB consumed by application
-            //result.RAM = Math.Round(ram.NextValue() / 1024 / 1024, 2);
+        //    // If system has multiple cores, that should be taken into account
+        //    result.CPU = Math.Round(cpu.NextValue() / Environment.ProcessorCount, 2);
+        //    // Returns number of MB consumed by application
+        //    //result.RAM = Math.Round(ram.NextValue() / 1024 / 1024, 2);
 
-            return result.CPU;
-        }
+        //    return result.CPU;
+        //}
     }
 }
